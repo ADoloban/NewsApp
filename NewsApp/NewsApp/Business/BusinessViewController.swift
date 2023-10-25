c255afb84272b159f77ede666de37c941eeac203
Artem Doloban <>
2023-10-25 11:32:26 +0300

    Initial commit

---
+++ NewsApp/NewsApp/Business/BusinessViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BusinessViewController.swift
+//  NewsApp
+//
+//  Created by Artem Doloban on 23.10.2023.
+//
+
+import UIKit
+
+final class BusinessViewController: UIViewController {
+    //MARK: - GUI Variables
+    //MARK: - Properties
+    
+    //MARK: - Private methods
+    //MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
---
+++ NewsApp/NewsApp/Technology/TechnologyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TechnologyViewController.swift
+//  NewsApp
+//
+//  Created by Artem Doloban on 23.10.2023.
+//
+
+import UIKit
+
+final class TechnologyViewController: UIViewController {
+    //MARK: - GUI Variables
+    //MARK: - Properties
+    
+    //MARK: - Private methods
+    //MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+}
