5197ad44e2f50408771940314e00079d44fb420f
Artem Doloban <>
2023-11-18 17:16:38 +0200

    Homework-2 commit

--- NewsApp/NewsApp/Business/View/BusinessViewController.swift
+++ NewsApp/NewsApp/Business/View/BusinessViewController.swift
@@ -12,8 +12,6 @@ class BusinessViewController: UIViewController {
 
     //MARK: - GUI Variables
    
-    
-    
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
 
@@ -24,8 +22,7 @@ class BusinessViewController: UIViewController {
                                            bottom: 20,
                                            right: 20)
         
-        let collectionView = UICollectionView(frame: CGRect(x: 
-                                                                0,
+        let collectionView = UICollectionView(frame: CGRect(x: 0,
                                                             y: 0,
                                                             width: view.frame.width,
                                                             height: view.frame.height),
@@ -37,19 +34,44 @@ class BusinessViewController: UIViewController {
     }()
     
     //MARK: - Properties
-    
+    private var viewModel: BusinessViewModelProtocol
     
     //MARK: - Life Cycle
+    init(viewModel: BusinessViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        self.setupViewModel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
         collectionView.register(GeneralCollectionViewCell.self, forCellWithReuseIdentifier: "GeneralCollectionViewCell")
-        
         collectionView.register(DetailsCollectionViewCell.self, forCellWithReuseIdentifier: "DetailsCollectionViewCell")
+        
+        viewModel.loadData()
     }
 
     //MARK: - Private methods
+    private func setupViewModel() {
+        viewModel.reloadData = { [weak self] in
+            self?.collectionView.reloadData()
+        }
+        
+        viewModel.reloadCell = { [weak self] row in
+            self?.collectionView.reloadItems(at: [IndexPath(row: row, section: 0)])
+        }
+        
+        viewModel.showError = { error in
+            print(error)
+        }
+    }
+    
     private func setupUI() {
         view.backgroundColor = .white
         view.addSubview(collectionView)
@@ -69,34 +91,42 @@ class BusinessViewController: UIViewController {
 extension BusinessViewController: UICollectionViewDataSource {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        2
+        viewModel.numberOfCells > 1 ? 2 : 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        section == 0 ? 1 : 15
+        if viewModel.numberOfCells > 1 {
+            return section == 0 ? 1 : viewModel.numberOfCells - 1
+        }
+        return viewModel.numberOfCells
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        var cell: UICollectionViewCell?
         
         if indexPath.section == 0 {
-            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GeneralCollectionViewCell",
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GeneralCollectionViewCell",
                                                                 for: indexPath) as? GeneralCollectionViewCell
+            let article = viewModel.getArticle(for: 0)
+            cell?.set(article: article)
+            
+            return cell ?? UICollectionViewCell()
         } else {
-            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DetailsCollectionViewCell",
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DetailsCollectionViewCell",
                                                       for: indexPath) as? DetailsCollectionViewCell
+            let article = viewModel.getArticle(for: indexPath.row + 1)
+            cell?.set(article: article)
+            
+            return cell ?? UICollectionViewCell()
         }
-        
-    
-        return cell ?? UICollectionViewCell()
     }
 }
 
 //MARK: - UICollectionViewDelegate
 extension BusinessViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        //navigationController?.pushViewController(NewsViewController(), animated: true)
+        let article = viewModel.getArticle(for: indexPath.section == 0 ? 0 : indexPath.row + 1)
+        navigationController?.pushViewController(NewsViewController(viewModel: NewsViewModel(article: article)), animated: true)
     }
 }
 
--- NewsApp/NewsApp/Business/ViewModel/BusinessViewModel.swift
+++ NewsApp/NewsApp/Business/ViewModel/BusinessViewModel.swift
@@ -6,3 +6,76 @@
 //
 
 import Foundation
+
+protocol BusinessViewModelProtocol {
+    var reloadData: (() -> Void)? { get set }
+    var showError: ((String) -> Void)? { get set }
+    var reloadCell: ((Int) -> Void)? { get set }
+    
+    var numberOfCells: Int { get }
+    
+    func loadData()
+    func getArticle(for row: Int) -> ArticleCellViewModel
+}
+
+final class BusinessViewModel: BusinessViewModelProtocol {
+    var reloadData: (() -> Void)?
+    var reloadCell: ((Int) -> Void)?
+    var showError: ((String) -> Void)?
+    
+    //MARK: - Properties
+    private var articles: [ArticleCellViewModel] = [] {
+        didSet {
+            DispatchQueue.main.async{
+                self.reloadData?()
+            }
+        }
+    }
+    
+    var numberOfCells: Int {
+        articles.count
+    }
+    
+    func getArticle(for row: Int) -> ArticleCellViewModel {
+        articles[row]
+    }
+    
+    func loadData() {
+        ApiManager.getNews(from: .business) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let articles):
+                self.articles = self.convertToCellViewModel(articles)
+                self.loadImage()
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.showError?(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    private func convertToCellViewModel(_ articles: [ArticleResponceObject]) -> [ArticleCellViewModel] {
+        return articles.map { ArticleCellViewModel(article: $0) }
+    }
+    
+    private func loadImage() {
+        
+        for(index, article) in articles.enumerated() {
+            ApiManager.getImageData(url: article.imageUrl) { [weak self] result in
+                
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(let data):
+                        self?.articles[index].imageData = data
+                        self?.reloadCell?(index)
+                    case .failure(let error):
+                        self?.showError?(error.localizedDescription)
+                    }
+                }
+            }
+        }
+    }
+
+}
